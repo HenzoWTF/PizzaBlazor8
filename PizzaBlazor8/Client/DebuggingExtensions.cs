0b25edbe878f228fc0236f06e47f23339eea6b42
HenzoWTF <Urenahenzo@gmail.com>
2024-02-04 13:38:15 -0400

    Terminado

---
+++ PizzaBlazor8/Client/DebuggingExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices.ObjectiveC;
+using System.Text.Json;
+
+namespace PizzaBlazor8.Client
+{
+    public class DebuggingExtensions
+    {
+        private static JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+        public static string ToJson (Object obj) => JsonSerializer.Serialize(obj, options);
+    }
+}
